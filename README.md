db0bed4478a2ddc77f912fe679fe5a5dbc72b81a
alex-battikha <56514843+alex-battikha@users.noreply.github.com>
2020-09-25 20:30:46 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Collision-Bounce-Off-Detection-Algorithm
-An algorithm to detect the collision of sprites. This algorithm was later used to create a bounce-off sprite algorithm.
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,35 @@
+var movingRect;
+var fixedRect;
+
+function setup() {
+  createCanvas(1200,800);
+  movingRect = createSprite(400, 100, 50, 80);
+  movingRect.shapeColor = "black";
+  movingRect.debug = true;
+  fixedRect = createSprite(400, 800, 80, 30);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+
+  movingRect.velocityY = 6;
+  fixedRect.velocityY = -6;
+}
+
+function draw() {
+  background(0,0,0);
+  
+  if(movingRect.x - fixedRect.x < movingRect.width/2 + fixedRect.width/2 &&
+    fixedRect.x - movingRect.x < movingRect.width/2 + fixedRect.width/2){
+      movingRect.velocityX = movingRect.velocityX*(-1);
+      fixedRect.velocityX = fixedRect.velocityX*(-1);
+    }
+
+  if(
+    movingRect.y - fixedRect.y < movingRect.height/2 + fixedRect.height/2 &&
+    fixedRect.y - movingRect.y < movingRect.height/2 + fixedRect.height/2){
+      movingRect.velocityY = movingRect.velocityY*(-1);
+      fixedRect.velocityY = fixedRect.velocityY*(-1);
+  }
+
+  drawSprites();
+}
+
